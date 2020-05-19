b0d7aa4ad8487637effb119b5b97dbc37904a82e
Kuddleman <edl1155@gmail.com>
2020-05-19 07:40:38 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//function to create a store:
+
+function createStore() {
+
+  // The store must have 4 parts:
+  // 1. Contain the State (just create variable to to that)
+
+  
+  // 2. Get the state 
+  // 3. Listen to changes on the state. 
+  // 4. Update the state.
+
+}
