3bcb836d0c69ee27021f31c3c7f9a68d95fc69b7
Kuddleman <edl1155@gmail.com>
2020-05-19 07:48:31 -0700

    set up store

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ function createStore() {
 
   // The store must have 4 parts:
   // 1. Contain the State (just create variable to to that)
+  let state;
 
-  
-  // 2. Get the state 
+  // 2. Get the state. create function that just returns state
+  const getState = () => state
   // 3. Listen to changes on the state. 
   // 4. Update the state.
+
+  //#2 continued:  This return statment exposed the getState() function
+  return (
+    getState
+  )
 
 }
