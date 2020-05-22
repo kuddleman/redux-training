cee05d0def096ba64b320e819debebc3162eda02
Kuddleman <edl1155@gmail.com>
2020-05-22 12:35:06 -0700

    add reducer

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 //function to create a store:
 
+// STEP 4. Update the state. Here are our rules:
+    // 1. Only an event can change the state of the store:
+    // What is an event? Put events into OBJECTS and call them ACTIONS
+    // 2.  The function that returns the new state needs to be a pure function to maintain predictability
+          //a. a pure function returns the same result if the same arguments are passed in.
+          //b. a pure function depends solely on the arguments passed in to them
+               //ex.  it does not access global variable
+          //c. a pure function does not product side effects.  Ex. no interaction with outside world...no API requests.
+//this function is a reducer:  it takes in previous state and an action and reduces it down to a new state.
+// a REDUCER MUST BE A PURE FUNCTION
+function todos ( state = [], action ) {
+  if ( action.type === 'ADD_TODO' ) {
+    return state.concat([ action.todo ])
+  }
+
+  return state
+}
+
 function createStore() {
 
   // The store must have 4 parts:
-  // 1. Contain the State (just create variable to to that)
+  // STEP 1. Contain the State (just create variable to to that)
   let state;
   let listeners = []
 
-  // 2. Get the STATE TREE. create function that just returns state
+  // STEP 2. Get the STATE TREE. create function that just returns state
   const getState = () => state
 
 
-  // 3. Listen to changes on the state. Give users a way to listen to changes
+  // STEP 3. Listen to changes on the state. Give users a way to listen to changes
   const subscribe = () => {
     listeners.push( listener )
 
@@ -20,10 +38,7 @@ function createStore() {
       listeners = listeners.filter(l => l !== listener)
     }
   }
-  // 4. Update the state. Here are our rules:
-  // Only an event can change the state of the store:
-  // What is an event? Put events into OBJECTS and call them ACTIONS
-
+  
   //#2 continued:  This return statment exposed the getState() function
   return (
     getState
